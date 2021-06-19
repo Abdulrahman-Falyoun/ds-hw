1d1272c8e14cee326276d02add2ca0bf61f8a932
AbdulrahmanFalyounScand <abd.falyoun@scandinaviatech.com>
2021-06-19 11:54:03 +0300

    [FIX]: adding jeager for tracing

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -7,6 +7,7 @@ import { FileUploadModule } from '../../../libs/file-upload/src';
 import { MainRequestModule } from './request/main.request.module';
 import { DiscoveryService, EurekaModule } from 'nestjs-eureka';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TracingModule } from '@dollarsign/nestjs-jaeger-tracing/dist';
 
 @Module({
   imports: [
@@ -20,6 +21,40 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       load: [configuration],
     }),
     FileUploadModule,
+    // TracingModule.forRoot({
+    //   exporterConfig: {
+    //     serviceName: 'core-service',
+    //     // port: 16686, 9411
+    //     port: 16686,
+    //     host: 'localhost',
+    //   },
+    //   isSimpleSpanProcessor: true,
+    // }),
+
+    ClientsModule.register([
+      {
+        name: 'GATEWAY_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          ...TracingModule.getParserOptions(),
+        },
+      },
+      {
+        name: 'IMAGE_HANDLER_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          ...TracingModule.getParserOptions(),
+        },
+      },
+    ]),
+    TracingModule.forRootAsync({
+      useFactory: () => ({
+        exporterConfig: {
+          serviceName: 'gateway-service',
+        },
+        isSimpleSpanProcessor: true,
+      }),
+    }),
   ],
   controllers: [],
   providers: [],
--- apps/image-handler/src/image-handler.module.ts
+++ apps/image-handler/src/image-handler.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ImageHandlerController } from './image-handler.controller';
 import { ImageHandlerService } from './image-handler.service';
 import { ConfigModule } from '@nestjs/config';
@@ -7,6 +7,8 @@ import { DatabaseModule } from '../../../libs/database/src';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DS_FILE_SCHEMA } from '../../schema-names';
 import { FileSchema } from '../../../libs/file-upload/src';
+import { TracingModule } from '@dollarsign/nestjs-jaeger-tracing/dist';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -22,6 +24,16 @@ import { FileSchema } from '../../../libs/file-upload/src';
       load: [configuration],
     }),
     DatabaseModule,
+    TracingModule.forRoot({
+      exporterConfig: {
+        serviceName: 'image-handler-service',
+      },
+      isSimpleSpanProcessor: true,
+    }),
+    ClientsModule.register([
+      { name: 'IMAGE_HANDLER_SERVICE', transport: Transport.REDIS },
+    ]),
+
   ],
   controllers: [ImageHandlerController],
   providers: [ImageHandlerService],
